a17313dd75907144c4dd94ed89dc8afc899d8f2b
a-ko0620 <akovalchuknf@gmail.com>
2020-06-15 23:11:37 +0400

    Коммит для задания №2

--- sandbox/src/main/java/ru/spqr/sandbox/FirstHello.java
+++ sandbox/src/main/java/ru/spqr/sandbox/FirstHello.java
@@ -2,7 +2,40 @@ package ru.spqr.sandbox;
 
 public class FirstHello {
 
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        /**Первый расчет**/
+        System.out.println(distance1());
+
+        /**Второй расчет**/
+        System.out.println(distance2());
+
+    }
+
+    /**
+     * Первый расчет
+     **/
+    public static double distanceX(int x1, int x2) {
+        return x2 - x1;
+    }
+
+    public static double distanceY(int y1, int y2) {
+        return y2 - y1;
+    }
+
+    public static double distance1() {
+        return Math.sqrt(Math.pow((distanceX(1, 5)), 2) + Math.pow((distanceY(1, 4)), 2));
+    }
+
+    /**
+     * Второй расчет
+     **/
+    public static double distance2() {
+
+        Point1 p1 = new Point1(1, 1);
+        Point2 p2 = new Point2(5, 4);
+
+        return Math.sqrt(Math.pow((p2.x2 - p1.x1), 2) + Math.pow((p2.y2 - p1.y1), 2));
+    }
 }
