6059b3dc243c3aabaa20fdf35adcbff74e950afb
a-ko0620 <akovalchuknf@gmail.com>
2020-06-16 13:08:31 +0400

    Добавление единого класса Point. Удаление 2х предыдущих классов

--- sandbox/src/main/java/ru/spqr/sandbox/FirstHello.java
+++ sandbox/src/main/java/ru/spqr/sandbox/FirstHello.java
@@ -5,26 +5,22 @@ public class FirstHello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
 
+        double d;
+
+         Point p1 = new Point(1, 1);
+         Point p2 = new Point(5, 4);
+
+         d = distance(p1 ,p2);
+
         /**Первый расчет**/
-        int x1 = 1;
-        int x2 = 5;
-        int y1 = 1;
-        int y2 = 4;
-        System.out.println("Расстояние между точками " + Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) + " единиц");
+        System.out.println("Расстояние между точками, используя статический метод в этом классе " + d + " единиц");
 
         /**Второй расчет**/
-        System.out.println("Расстояние между точками " + distance1() + " единиц");
+        System.out.println("Расстояние между точками, используя метод в классе Point " + p1.distance(p2) + " единиц");
 
     }
 
-    /**
-     * Второй расчет
-     **/
-    public static double distance1() {
-
-        Point1 p1 = new Point1(1, 1);
-        Point2 p2 = new Point2(5, 4);
-
-        return Math.sqrt(Math.pow((p2.x2 - p1.x1), 2) + Math.pow((p2.y2 - p1.y1), 2));
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 }
