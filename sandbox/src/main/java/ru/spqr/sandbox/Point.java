97d23928bc0c233a7a4d9d67ad150fde7918ac5c
a-ko0620 <akovalchuknf@gmail.com>
2020-06-16 13:09:11 +0400

    Revert "Revert "Добавление единого класса Point. Удаление 2х предыдущих классов""
    This reverts commit 4eb8eab4970b96f5c2f32f7f2510b87fbf85ce6e.

---
+++ sandbox/src/main/java/ru/spqr/sandbox/Point.java
@@ -0,0 +1,18 @@
+package ru.spqr.sandbox;
+
+public class Point {
+
+    public double x;
+    public double y;
+
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow((p.x - this.x), 2) + Math.pow((p.y - this.y), 2));
+    }
+}
